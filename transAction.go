10d1a1a4cc907ca4591462f0bd0d4a7fe9626a3a
delighted322 <gsy322@163.com>
2018-10-20 21:50:54 +0800

    v5-part2_06 实现FindTransactionByTXid 实现SignTransaction函数(封装)

--- transAction.go
+++ transAction.go
@@ -172,33 +172,7 @@ func NewTransaction(from, to string, amount float64, bc *BlockChain) *TransActio
 	tx := TransAction{[]byte{},inputs,outputs}
 	tx.SetHash()
 
-	//签名 交易创建的最后进行签名
-	prevTXs := make(map[string]TransAction)
-
-	//找到所有引用的交易
-	//1. 根据inputs来找，有多少input, 就遍历多少次
-	//2. 找到目标交易，（根据TXid来找）
-	//3. 添加到prevTXs里面
-	for _,input := range tx.TXInputs {
-		//根据id查找交易本身 需要遍历整个区块链
-		tx := FindTransactionByTxid(input.TXid)
-
-		prevTXs[string(input.TXid)] = tx
-		//第一个input查找之后：prevTXs：
-		// map[2222]Transaction222
-
-		//第二个input查找之后：prevTXs：
-		// map[2222]Transaction222
-		// map[3333]Transaction333
-
-		//第三个input查找之后：prevTXs：
-		// map[2222]Transaction222
-		// map[3333]Transaction333(只不过是重新写了一次)
-	}
-
-
-	tx.Sign(*privateKey,prevTXs)
-
+	bc.SignTransaction(&tx,privateKey)
 	return  &tx
 }
 
@@ -206,7 +180,7 @@ func NewTransaction(from, to string, amount float64, bc *BlockChain) *TransActio
 // 参数为：私钥，inputs里面所有引用的交易的结构map[string]Transaction
 //map[2222]Transaction222
 //map[3333]Transaction333
-func (tx *TransAction ) Sign(privateKey ecdsa.PrivateKey,prevTXs map[string]TransAction)  {
+func (tx *TransAction ) Sign(privateKey *ecdsa.PrivateKey,prevTXs map[string]TransAction)  {
 	//具体签名的动作先不管 稍后继续
 
 	//TODO
