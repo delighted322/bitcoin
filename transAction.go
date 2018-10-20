809afbc6ccd09db3a7d7ecb8c664dfd55b67717c
delighted322 <gsy322@163.com>
2018-10-20 21:33:22 +0800

    v5-part2_05 根据inputs查找prevTXs

--- transAction.go
+++ transAction.go
@@ -175,6 +175,28 @@ func NewTransaction(from, to string, amount float64, bc *BlockChain) *TransActio
 	//签名 交易创建的最后进行签名
 	prevTXs := make(map[string]TransAction)
 
+	//找到所有引用的交易
+	//1. 根据inputs来找，有多少input, 就遍历多少次
+	//2. 找到目标交易，（根据TXid来找）
+	//3. 添加到prevTXs里面
+	for _,input := range tx.TXInputs {
+		//根据id查找交易本身 需要遍历整个区块链
+		tx := FindTransactionByTxid(input.TXid)
+
+		prevTXs[string(input.TXid)] = tx
+		//第一个input查找之后：prevTXs：
+		// map[2222]Transaction222
+
+		//第二个input查找之后：prevTXs：
+		// map[2222]Transaction222
+		// map[3333]Transaction333
+
+		//第三个input查找之后：prevTXs：
+		// map[2222]Transaction222
+		// map[3333]Transaction333(只不过是重新写了一次)
+	}
+
+
 	tx.Sign(*privateKey,prevTXs)
 
 	return  &tx
